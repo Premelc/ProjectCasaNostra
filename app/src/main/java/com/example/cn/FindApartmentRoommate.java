48432b309ede88614fc3bcd7e0bcef31a9652551
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2021-04-17 12:12:35 +0200

    Add files via upload
    Napravljene aktivnosti: login, register, obrazac za upoznavanje želja korisnika, obrasci koje korisnik ispunjava za odabir cimera/cimera i stana

---
+++ app/src/main/java/com/example/cn/FindApartmentRoommate.java
@@ -0,0 +1,38 @@
+package com.example.cn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+public class FindApartmentRoommate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_apartment_roommate);
+
+    }
+
+    public void onlyRoommate(View v){
+        RadioButton roommate = (RadioButton)v;
+        if(roommate.isChecked()){
+            Intent next = new Intent(this, OnlyRoommate.class);
+            startActivity(next);
+        }
+    }
+
+    public void roommateAndApartment(View v){
+        RadioButton roommate = (RadioButton)v;
+        if(roommate.isChecked()){
+            Intent next = new Intent(this, ApartmentAndRoommate.class);
+            startActivity(next);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/cn/Login.java
@@ -0,0 +1,49 @@
+package com.example.cn;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        /* Return arrow */
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    /* Return arrow*/
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    /*Redirect on home page after successful login */
+    public void homePage(View v){
+        Intent hPage = new Intent(this, HomePage.class);
+        startActivity(hPage);
+    }
+
+
+
+}
