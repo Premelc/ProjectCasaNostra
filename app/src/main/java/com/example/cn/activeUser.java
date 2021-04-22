e3c87289953d9082c9ed17c9cf96057a368c1c2f
Premelc <dominik.premelc33@gmail.com>
2021-04-22 19:10:09 +0200

    Dodane klase za aktivnog korisnika i korisnike koji se nude aktivnom korisniku

---
+++ app/src/main/java/com/example/cn/activeUser.java
@@ -0,0 +1,32 @@
+package com.example.cn;
+
+import com.example.cn.model.Korisnik;
+
+public class activeUser extends Korisnik {
+    private String username;
+    private String email;
+    private String password;
+
+    public activeUser(){
+        super();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/cn/otherUser.java
@@ -0,0 +1,9 @@
+package com.example.cn;
+
+import com.example.cn.model.Korisnik;
+
+public class otherUser extends Korisnik {
+    public otherUser(){
+        super();
+    }
+}
