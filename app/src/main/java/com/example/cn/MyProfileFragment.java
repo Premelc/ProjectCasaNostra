9fdb25951e0747ad560f2e1fed034b70851c1b94
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2021-05-18 01:04:38 +0200

    Pomocu Firebase-a omoguceno je dodavanje profilne slike korisnika. Omogucen je odabir i upload  slike na udaljenu bazu te prikaz slike profila.

--- app/src/main/java/com/example/cn/MyProfileFragment.java
+++ app/src/main/java/com/example/cn/MyProfileFragment.java
@@ -1,13 +1,20 @@
 package com.example.cn;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,9 +25,22 @@ import androidx.fragment.app.Fragment;
 import com.example.cn.helpers.SaveSharedPreference;
 import com.example.cn.model.Korisnik;
 import com.example.cn.sql.DatabaseHelper;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
+
 
 public class MyProfileFragment extends Fragment {
 
+    private static final int RESULT_OK = -1;
     // Objekti
     DatabaseHelper databaseHelper;
     Korisnik sessionUser;
@@ -29,12 +49,26 @@ public class MyProfileFragment extends Fragment {
     private Button logout;
     private TextView deleteAccount;
 
+    // Buttoni itd. za Firebase
+    private Button btnChoose, btnUpload;
+    private ImageView imageView;
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+
+    //Firebase - za choose i upload
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    // Za dohvacanje
+    private StorageReference mStorageReference;
+
+    private boolean flag = false;
+
     public static MyProfileFragment newInstance(Korisnik user){
         MyProfileFragment fragment = new MyProfileFragment();
         Bundle bundle = new Bundle();
         bundle.putSerializable("Session", user);
         fragment.setArguments(bundle);
-
         return fragment;
     }
 
@@ -42,7 +76,6 @@ public class MyProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         sessionUser = (Korisnik) getArguments().getSerializable("Session");
-
         return inflater.inflate(R.layout.fragment_my_profile, container, false);
     }
 
@@ -54,6 +87,57 @@ public class MyProfileFragment extends Fragment {
 
         initViews();
         initListeners();
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        btnChoose = (Button) getView().findViewById(R.id.btnChoose);
+        btnUpload = (Button) getView().findViewById(R.id.btnUpload);
+
+        imageView = (ImageView) getView().findViewById(R.id.imgView);
+
+        // Ime slike sam stavila usr + broj jer za ime filea mora biti najmanje duzine 3
+        int idOfUser = sessionUser.getId_korisnik();
+        String number = Integer.toString(idOfUser);
+        String nameOfPic = "usr" + number;
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("images/usr"+ idOfUser);
+
+        try {
+            final File localFile = File.createTempFile(nameOfPic, "jpg");
+            mStorageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            //Toast.makeText(getActivity(), "Slika je dohvacena", Toast.LENGTH_SHORT).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+
+                            //  !!!!!!
+                            ((ImageView) getView().findViewById(R.id.imgView)).setImageBitmap(bitmap);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(), "Slika nije dohvacena", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+                flag = true;
+            }
+        });
     }
 
     private void initViews(){
@@ -123,4 +207,76 @@ public class MyProfileFragment extends Fragment {
         alert.show();
     }
 
+    // Za sliku
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private void uploadImage() {
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Promijeniti naziv slike - pomocu id-a
+            int idOfUser = sessionUser.getId_korisnik();
+            /*
+            String usr = "user" + idOfUser;
+            int i = 1;
+            StorageReference ref = storageReference.child("images/usr/"+ idOfUser + "_" + i);*/
+
+            StorageReference ref = storageReference.child("images/usr"+ idOfUser);
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            //Toast.makeText(MyProfileFragment.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            // Toast.makeText(MyProfileFragment.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+
+            // Ovdje bi trebalo postaviti da se ta odabrana slika savea u imageViewu
+        }
+    };
 }
+
+
