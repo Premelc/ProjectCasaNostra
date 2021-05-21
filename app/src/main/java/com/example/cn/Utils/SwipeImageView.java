e42b9a702495a681bc56b55f3eedcda57668b31f
Anđela Jaković <andelajakovic99@gmail.com>
2021-05-21 22:16:10 +0200

    Prikaz 4 slike na Swipe fragmentu i mogućnost navigacije među istima. Dodane su dvije nove veličine ImageView-a (1:1 za upload i 3:4 za swipe)

---
+++ app/src/main/java/com/example/cn/Utils/SwipeImageView.java
@@ -0,0 +1,41 @@
+package com.example.cn.Utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SwipeImageView extends AppCompatImageView {
+    public SwipeImageView(@NonNull @NotNull Context context) {
+        super(context);
+    }
+
+    public SwipeImageView(@NonNull @NotNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeImageView(@NonNull @NotNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width;
+        int height;
+        if(widthMeasureSpec < heightMeasureSpec){
+            height = getMeasuredHeight();
+            width = Math.round(height * .75f);
+        } else{
+            width = getMeasuredWidth();
+            height = Math.round(width * 1.25f);
+        }
+
+        setMeasuredDimension(width, height);
+    }
+}
