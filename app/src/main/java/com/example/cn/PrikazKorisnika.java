e571e9bff2fc683665fe0cd8d084d8b15f4ffc1a
denis <luka.zupanovic@gmail.com>
2021-05-30 11:21:40 +0200

    admin-tablicni prikaz korisnika

---
+++ app/src/main/java/com/example/cn/PrikazKorisnika.java
@@ -0,0 +1,89 @@
+package com.example.cn;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cn.model.Korisnik;
+import com.example.cn.sql.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PrikazKorisnika extends AppCompatActivity {
+    RecyclerView rv;
+    Button detaljiButton;
+    String s1[] = {"Baza podataka"};
+    String s2[] = {"Prikaz korisnika"};
+    int images[] = {1,2,3,4,5,6,7};
+    private DatabaseHelper databaseHelper = new DatabaseHelper(this);
+
+
+
+    Korisnik user;
+    List<Korisnik> userList = new ArrayList<Korisnik>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prikaz_korisnika);
+        rv = findViewById(R.id.rv);
+        detaljiButton = (Button) findViewById(R.id.detalji);
+        int p1 = 0,p2 = 0;
+        Log.d("aa","ss");
+        userList.clear();
+        userList.addAll(databaseHelper.queryKorisnik(null, null, null, null, null)); // dohvacanje korisnika po username-u
+
+        if(!userList.isEmpty()) {
+            for (int i = 0; i < userList.size(); i++) {
+                s1[i] = "Username: " + userList.get(i).getUsername();
+                s2[i] = "Email: " + userList.get(i).getEmail();
+                Log.d("aa", s1[i]);
+                final Korisnik a = userList.get(i);
+                detaljiButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(PrikazKorisnika.this);
+
+                        builder.setMessage("ID: " + a.getId_korisnik() + "\n" +
+                                "Godina rođenja: " + a.getGodina_rodenja() + "\n" +
+                                "Spol: " + a.getSpol() + "\n" +
+                                "ID fakulteta: " + a.getId_fakultet() + "\n" +
+                                "Spol cimera: " + a.getCimer_spol() + "\n")
+                                .setCancelable(false)
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+
+                                    }
+                                });
+
+                        AlertDialog alert = builder.create();
+
+                        alert.setTitle("DETALJI");
+                        alert.show();
+                    }
+
+                });
+            }
+        }else{
+            Log.d("abb", "dfsosdfji");
+            s1[0] = "Nema korisnika";
+            s2[0] = "Baza prazna";
+        }
+
+
+
+        RvAdaptor myAdaptor = new RvAdaptor(this,s1,s2,images);
+        rv.setAdapter(myAdaptor);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/cn/RvAdaptor.java
@@ -0,0 +1,61 @@
+package com.example.cn;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RvAdaptor extends RecyclerView.Adapter<RvAdaptor.MyViewHolder> {
+    String s1[],s2[];
+    int images[];
+    Context c;
+
+    public  RvAdaptor(Context ct, String s1[], String s2[], int images[]){
+        c = ct;
+        this.s1 = s1;
+        this.s2 = s2;
+        this.images = images;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater inf = LayoutInflater.from(c);
+        View v = inf.inflate(R.layout.myrow,parent,false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RvAdaptor.MyViewHolder holder, int position) {
+    holder.t1.setText(s1[position]);
+    holder.t2.setText(s2[position]);
+    //TODO:ovdje sliku jos doadat koristiti holder.myImage.setImageResource
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return s1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView t1,t2;
+        ImageView img1;
+        public MyViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            t1 = itemView.findViewById(R.id.pktxt);
+            t2 = itemView.findViewById(R.id.pktxt2);
+           // img1 = itemView.findViewById(R.id.pkimg);
+        }
+    }
+}
